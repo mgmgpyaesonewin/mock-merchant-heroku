cb6088011d359a4c0cf54686aee683e88a9ce347
Htet Wai Yan Soe <johnthelinux@gmail.com>
2019-12-24 17:28:09 +0630

    fix: callback request into storage

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 Route::any("callback", function(Request $request) {
-    Log::channel('callback')->error($request->all());
+    Storage::append('callback.log', json_encode($request->all()));
 });
 
 Route::post('add-to-cart', function(Request $request) {
