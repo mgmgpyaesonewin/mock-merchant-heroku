aa7e4f67c8553aba997d7c3de34b69367b6c8956
Pyae Sone Win <pyaesone.win@wavemoney.com.mm>
2022-12-12 14:21:54 +0630

    wip

---
+++ resources/js/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router@3';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    linkExactActiveClass: 'active',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+        },
+    ]
+});
+
+export default router;
