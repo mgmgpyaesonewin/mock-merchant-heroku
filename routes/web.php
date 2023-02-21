0d3798072b52e7dede8509747cc486a61d2d3cc8
Pyae Sone Win <pyaesone.win@wavemoney.com.mm>
2023-02-21 10:29:18 +0630

    laravel 10 initialized

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Route;
 
-Route::any("callback", "HomeController@callback");
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::post('add-to-cart', function(Request $request) {
-    session()->push('items',['name' => $request->name, 'amount' => $request->amount]);
-    flash($request->name . " is added to Cart!", 'alert alert-success');
-    return back();
-})->name('add-to-cart');
-
-Route::get('clear-cart', function(Request $request) {
-    session()->flush();
-    return back();
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Route::get('web/checkout', "HomeController@webCheckout");
-
-Route::get('api/checkout', "HomeController@apiCheckout");
-Route::post('api/checkout', "HomeController@postApiCheckout");
-
-
-Route::get('/', 'HomeController@index')->name('home');
-Route::post('/paymentInfo', 'HomeController@checkStatus')->name('paymentInfo');
-Route::post('/reversal/transaction', 'HomeController@reversalTransaction')->name('paymentInfo');
